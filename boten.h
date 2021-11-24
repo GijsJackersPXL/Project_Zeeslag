b7f795b7551170a1954d3a591c786fda64cb2402
MartijnGuilliamsPXL <56915229+MartijnGuilliamsPXL@users.noreply.github.com>
2021-11-24 10:37:40 +0100

    update boten.h

--- boten.h
+++ boten.h
@@ -6,10 +6,39 @@
 
 class boten
 {
+        virtual int breedte() = 0;
+
+        /*
 	public enum boten {
                 vliegdekschip, slagschip, Torpedobootjager, Patrouilleschip;
         }
-	
+	*/
 };
 
-#endif  BOTEN_H
+#endif // BOTEN_H
+
+/*
+
+#include<iostream>
+using namespace std;
+  
+class Base
+{
+public:
+    virtual void show() = 0;
+};
+  
+class Derived: public Base
+{
+public:
+    void show() { cout << "In Derived \n"; }
+};
+  
+int main(void)
+{
+    Base *bp = new Derived();
+    bp->show();
+    return 0;
+}
+
+*/
