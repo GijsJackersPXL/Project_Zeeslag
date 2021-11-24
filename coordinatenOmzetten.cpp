2e7083f7d232e971b2f2b1ce37f500c4fbc6ba7f
GijsJackersPXL <gijs.jackers@student.pxl.be>
2021-11-24 10:37:45 +0100

    Grid printen

--- coordinatenOmzetten.cpp
+++ coordinatenOmzetten.cpp
@@ -10,14 +10,15 @@ using namespace std;
 
 int main( int argumentCount, char * argumentVector[] )
 {
-    cout << "Geef een coordinaat op " << endl;
+   /* cout << "Geef een coordinaat op " << endl;
     cout << "Kies een coordinaat(A-J)(0-9): ";
     string coordinaat;
     getline(cin, coordinaat);
     //cout << "Je hebt coordinaat: " << coordinaat << " gekozen\n";
 
     char ch = coordinaat[0];
-    cout << int(toupper(ch)) << "\n";
+    //cout << int(toupper(ch)) << "\n";
+
     while (coordinaat.length() != 2 || isalpha(coordinaat[0]) != 1 || isdigit(coordinaat[1]) != 1 || (64 < int(ch) && int(ch) < 75) != 1 )  //A = 65 // J=74 //ABCDEFGHIJ
     {
          cout << "Foute coordinaat\n";
@@ -27,6 +28,28 @@ int main( int argumentCount, char * argumentVector[] )
          getline(cin, coordinaat);
          ch = coordinaat[0];
          cout << int(toupper(ch)) << "\n";
-     }
+    }*/
+
     cout << "Je coordinaat is goedgekeurd" << endl;
+
+    bool geraakt = false;
+
+    //Grid printen
+    cout <<"  A B C D E F G H I J \n";
+    for (int i = 0; i <= 9; i++) // van 0-9
+    {
+        cout << i << " ";
+        for (int i = 0; i <= 9; i++) // van A-J
+        {
+            if (geraakt != true)
+            {
+                cout << "." << " ";
+            }
+            else
+            {
+                cout << "X" << " ";
+            }
+        }
+        cout << "\n";
+    }
 }
