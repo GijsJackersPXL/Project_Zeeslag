e3173d45dedbda8f5ee8ba9f90e6f0fd12dc2c9a
GijsJackersPXL <gijs.jackers@student.pxl.be>
2021-11-24 14:26:03 +0100

    update spel en zeeslag Co-authored-by: MartijnGuilliamsPXL <MartijnGuilliamsPXL@users.noreply.github.com>

--- spel.cpp
+++ spel.cpp
@@ -1,32 +1,33 @@
 #include "spel.h"
+using namespace std;
 
 
-void spel::intro()
+void Spel::intro()
 {
-    // printf("  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.    \n");
-    // printf(" | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |   \n");
-    // printf(" | |   ________   | || |  _________   | || |  _________   | || |    _______   | || |   _____      | || |      __      | || |    ______    | |   \n");
-    // printf(" | |  |  __   _|  | || | |_   ___  |  | || | |_   ___  |  | || |   /  ___  |  | || |  |_   _|     | || |     /  \     | || |  .' ___  |   | |   \n");
-    // printf(" | |  |_/  / /    | || |   | |_  \\_|  | || |   | |_  \\_|  | || |  |  (__ \\_|  | || |    | |       | || |    / /\\ \    | || | / .'   \\_|   | |   \n");
-    // printf(" | |     .'.' _   | || |   |  _|  _   | || |   |  _|  _   | || |   '.___`-.   | || |    | |   _   | || |   / ____ \\   | || | | |    ____  | |   \n");
-    // printf(" | |   _/ /__/ |  | || |  _| |___/ |  | || |  _| |___/ |  | || |  |`\\____) |  | || |   _| |__/ |  | || | _/ /    \\ \_ | || | \\ `.___]  _| | |   \n");
-    // printf(" | |  |________|  | || | |_________|  | || | |_________|  | || |  |_______.'  | || |  |________|  | || ||____|  |____|| || |  `._____.'   | |   \n");
-    // printf(" | |              | || |              | || |              | || |              | || |              | || |              | || |              | |   \n");
-    // printf(" | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |   \n");
-    // printf("  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'    \n");
+    cout << "  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.    \n";
+    cout << " | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |   \n";
+    cout << " | |   ________   | || |  _________   | || |  _________   | || |    _______   | || |   _____      | || |      __      | || |    ______    | |   \n";
+    cout << " | |  |  __   _|  | || | |_   ___  |  | || | |_   ___  |  | || |   /  ___  |  | || |  |_   _|     | || |     /  \\     | || |  .' ___  |   | |   \n";
+    cout << " | |  |_/  / /    | || |   | |_  \\_|  | || |   | |_  \\_|  | || |  |  (__ \\_|  | || |    | |       | || |    / /\\ \\    | || | / .'   \\_|   | |   \n";
+    cout << " | |     .'.' _   | || |   |  _|  _   | || |   |  _|  _   | || |   '.___`-.   | || |    | |   _   | || |   / ____ \\   | || | | |    ____  | |   \n";
+    cout << " | |   _/ /__/ |  | || |  _| |___/ |  | || |  _| |___/ |  | || |  |`\\____) |  | || |   _| |__/ |  | || | _/ /    \\ \\_ | || | \\ `.___]  _| | |   \n";
+    cout << " | |  |________|  | || | |_________|  | || | |_________|  | || |  |_______.'  | || |  |________|  | || ||____|  |____|| || |  `._____.'   | |   \n";
+    cout << " | |              | || |              | || |              | || |              | || |              | || |              | || |              | |   \n";
+    cout << " | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |   \n";
+    cout << "  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'    \n";
 
-    //printf("DOEL VAN HET SPEL: \n");
-    //printf("Doel is dat beide spelers zo door gericht te raden proberen als eerste de volledige locaties van alle schepen van de tegenstander te raden en zo \n);
-    //printf("de vloot van de tegenstander "tot zinken" te brengen. Wanneer alle coördinaten van een schip zijn geraakt, is het schip gezonken.\n);
+    cout << "DOEL VAN HET SPEL: \n";
+    cout << "Doel is dat beide spelers zo door gericht te raden proberen als eerste de volledige locaties van alle schepen van de tegenstander te raden en zo \n";
+    cout << "de vloot van de tegenstander 'tot zinken' te brengen. Wanneer alle coördinaten van een schip zijn geraakt, is het schip gezonken.\n\n";
 
-    //printf("HOE PLAATS JE EN BOOT \n");
-    //printf("Eerst gaat er gevraagd worden om je booten te plaatsen. De regels voor boten te plaatsen is dat de boten elkaar nooit mogen raken. \n");
-    //printf("Er gaat gevraagd worden voor de coordinaten van de 1ste boot. Hier geef je eerst een letter(A-J) daarna een cijfer(0-9) als je dat hebt ingegeven duw je op enter  \n");
-    //printf("Als de coordinaat goedgekeurd is zal er gevraagd worden of de boot horizontaal(h) of vertikaal(v) komt te staan, maak je keuze \n");
-    //printf("Je zal een goedkeuring krijgen als je boot gezet maar je kan ook een error krijgen als je boot verkeerd staat.\n");
+    cout << "HOE PLAATS JE EN BOOT \n";
+    cout << "Eerst gaat er gevraagd worden om je booten te plaatsen. De regels voor boten te plaatsen is dat de boten elkaar nooit mogen raken. \n";
+    cout << "Er gaat gevraagd worden voor de coordinaten van de 1ste boot. Hier geef je eerst een letter(A-J) daarna een cijfer(0-9) als je dat hebt ingegeven duw je op enter  \n";
+    cout << "Als de coordinaat goedgekeurd is zal er gevraagd worden of de boot horizontaal(h) of vertikaal(v) komt te staan, maak je keuze \n";
+    cout << "Je zal een goedkeuring krijgen als je boot gezet maar je kan ook een error krijgen als je boot verkeerd staat.\n\n";
     
-    //printf("HOE SPEEL JE HET SPEL IN DE CONSOLE: \n");
-    //printf("Dadelijk als je het spel gaat starten gaat er  \n");
+    cout << "HOE SPEEL JE HET SPEL IN DE CONSOLE: \n";
+    cout << "Dadelijk als je het spel gaat starten gaat er  \n";
 
     //...
 
