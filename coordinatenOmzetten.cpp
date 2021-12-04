7f8759ff07374c1bdc1e0cafb3e3e489f927b6e0
GijsJackersPXL <gijs.jackers@student.pxl.be>
2021-12-04 17:01:03 +0100

    Update coordinatenOmzetten.cpp
    - aangepast dat coordinaten worden opgeslagen en geprint worden
    - nog een fout in het printen/opslaan -> volgende keer zoeken
    
    Co-Authored-By: MartijnGuilliamsPXL <56915229+MartijnGuilliamsPXL@users.noreply.github.com>

--- coordinatenOmzetten.cpp
+++ coordinatenOmzetten.cpp
@@ -9,13 +9,17 @@ using namespace std;
 
 int main( int argumentCount, char * argumentVector[] )
 {
+    int geschotenPunten[9][9];
+    
     string stop = "";
-    while (stop != "y")
+    while (stop != "y" && stop != "Y")
     {
+        //coordinaat kiezen
         cout << "Kies een coordinaat(A-J)(0-9): ";
         string coordinaat;
         getline(cin, coordinaat);
 
+        //testen van coordinaat
         while (coordinaat.length() != 2 || isalpha(coordinaat[0]) != 1 || isdigit(coordinaat[1]) != 1 || (64 < int(coordinaat[0]) && int(coordinaat[0]) < 75) != 1 )  //A = 65 // J=74 //ABCDEFGHIJ
         {
             cout << "Foute coordinaat\n";
@@ -26,25 +30,40 @@ int main( int argumentCount, char * argumentVector[] )
 
         //coordinaat omzetten zodat deze geprint kan worden;
         char ch0 = coordinaat[0];
+        cout << "char: " << ch0;
         char ch1 = coordinaat[1];
-        int coordinaat0 = int(ch0) - 65;
-        int coordinaat1 = int(ch1) - 48;
+        cout << ch1 << "\n";
+        int coordinaatCijfer = int(ch0) - 65;
+        cout << "Coordinaat: " << coordinaatCijfer;
+        int coordinaatLetter = int(ch1) - 48;
+        cout << coordinaatLetter << "\n";
+        
+        geschotenPunten[coordinaatLetter][coordinaatCijfer] = 1;
 
         //Grid printen
         cout <<"  A B C D E F G H I J \n";
-        for (int i = 0; i <= 9; i++) // van 0-9
+        for (int C = 0; C <= 9; C++) // van 0-9
         {
-            cout << i << " ";
-            for (int j = 0; j <= 9; j++) // van A-J
+            cout << C << " ";
+            for (int L = 0; L <= 9; L++) // van A-J
             {
-                if (j == coordinaat0 && i == coordinaat1)
+                //if (geschotenPunten[L][C] == 1 || L == coordinaatCijfer && C == coordinaatLetter)
+                if (geschotenPunten[C][L] == 1)
+                {
+                    cout << "X" << " ";
+                }else
+                {
+                    cout << "." << " ";
+                }
+                
+                /*if (j == coordinaatCijfer && i == coordinaatLetter)
                 {
                     cout << "X" << " ";
                 }
                 else
                 {
                     cout << "." << " ";
-                }
+                }*/
             }
             cout << "\n";
         }
