e76882c02e2612787450202282ee1f74b570b47a
GijsJackersPXL <gijs.jackers@student.pxl.be>
2021-12-26 13:21:17 +0100

    kiezen grid

---
+++ kiezenGrid.h
@@ -0,0 +1,17 @@
+#ifndef KIEZENGRID_H
+#define KIEZENGRID_H
+
+#include <string>
+//#include "... .h"
+
+
+class kiezenGrid
+{
+        private int veld[10][10] = {0};
+        
+	public:
+        //speelveld()
+	
+};
+
+#endif  KIEZENGRID_H
